4b65cb6383c3ec46d2618bb54da737de2d5af970
Sabbir Rifat <rifat234dgh@gmail.com>
2021-06-03 00:48:30 +0600

    Search Page UI complete

---
+++ src/Utils/SearchData.js
@@ -0,0 +1,52 @@
+export const data = [
+    {
+        "link": "https://wearejames.com",
+        "title": "the official JAMES website",
+        "description": " All The Colours Of You. The New Album. Out 4th June."
+    },
+    {
+        "link": "https://wearejames.com",
+        "title": "Live | James - Wearejames.com",
+        "description": "Details of new album Girl At The End Of The World ft Nothing but Love, TO My Surprise, the title track & Dear John. Plus JAMES news, tour dates & official merch"
+    },
+    {
+        "link": "https://en.wikipedia.org",
+        "title": "James (band) - Wikipedia",
+        "description": " James and English rock band from Manchester, who were formed in 1982. They enjoyed popularity thoughout the 1990s. with four top 10s on the UK Singles.... "
+    },
+]
+
+export const availableStreamsData = [
+    {
+        "link": "https://youtube.com/jaketrent",
+        "title": "YouTube"
+    },
+    {
+        "link": "https://spotify.com/jaketrent",
+        "title": "Spotify"
+    },
+    {
+        "link": "https://twitter.com/jaketrent",
+        "title": "Twitter"
+    }
+]
+
+
+export const resultTabShortInfos = [
+    {
+        "description": "Whalley Range, Manchester, England",
+        "title": "Origin"
+    },
+    {
+        "description": "Indie pop; Madchester; alternative rock",
+        "title": "Genres"
+    },
+    {
+        "description": "Factory; Sire; Rough trade; Fortana; Mercury; Sanctuary; Decca; Cooking Vinyl; BMG",
+        "title": "Labels"
+    },
+    {
+        "description": "Tim Booth, Jim Glennie, Saul Davies",
+        "title": "Members"
+    }
+]
---
+++ src/components/Faq/Faq.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemPanel,
+  AccordionItemButton,
+} from "react-accessible-accordion";
+import "./Faq.styles.css";
+import "react-accessible-accordion/dist/fancy-example.css";
+
+const Faq = () => {
+  return (
+    <div className="faq-area-content">
+      <h3>People also ask</h3>
+      <Accordion>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              What harsh truths do you prefer to ignore?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat
+              occaecat ut occaecat consequat est minim minim esse tempor laborum
+              consequat esse adipisicing eu reprehenderit enim.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              Is free will real or just an illusion?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+              nostrud velit in irure cillum tempor laboris sed adipisicing eu
+              esse duis nulla non.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>
+              What's the catch with this offer?
+            </AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+              nostrud velit in irure cillum tempor laboris sed adipisicing eu
+              esse duis nulla non.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionItemHeading>
+            <AccordionItemButton>Are they really exists?</AccordionItemButton>
+          </AccordionItemHeading>
+          <AccordionItemPanel>
+            <p>
+              In ad velit in ex nostrud dolore cupidatat consectetur ea in ut
+              nostrud velit in irure cillum tempor laboris sed adipisicing eu
+              esse duis nulla non.
+            </p>
+          </AccordionItemPanel>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+
+export default Faq;
